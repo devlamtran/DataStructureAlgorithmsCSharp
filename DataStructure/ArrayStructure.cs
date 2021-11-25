0568bc7b1eb3be7992d345ea50b9093d808c509d
devlamtran <tranriemann5298@gmail.com>
2021-11-25 20:38:47 +0700

    frist commit

---
+++ DataStructure/ArrayStructure.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+namespace DataStructure
+{
+    public class ArrayStructure
+    {
+        //find element has value Maximum in array
+        //input : given an array
+        //output : return an element which has value Maximum in given array
+        public static int FindMaximumElementInArray(int[] input)
+        {
+            int output = input[0];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (output < input[i])
+                {
+                    output = input[i];
+                }
+            }
+            return output;
+        }
+
+        //find element has value Minimum in array
+        //input : given an array
+        //output : return an element which has value Minimum in given array
+        public static int FindMinimumElementInArray(int[] input)
+        {
+            int output = input[0];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (output > input[i])
+                {
+                    output = input[i];
+                }
+            }
+            return output;
+        }
+
+        //caculate sum value of element in array
+        //input : given an array
+        //output : return sum value of element in given array
+        public static int SumOfElementInArray(int[] input)
+        {
+            int sum = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                sum += sum + input[i];
+            }
+            return sum;
+        }
+
+        //delete element has value equal given value in array
+        //input : given an array, given element
+        //output : return an array after delete given element in given array
+        public static int[] DeleteElementInArray(int element, int[] input)
+        {
+            int countElementSameGivenElement = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == element)
+                {
+                    countElementSameGivenElement++;
+                }
+            }
+            if (countElementSameGivenElement == 0)
+            {
+                return input;
+            }
+            int[] indexElementSameGivenElements = new int[countElementSameGivenElement];
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == element)
+                {
+                    for (int j = 0; j < indexElementSameGivenElements.Length; j++)
+                    {
+                        indexElementSameGivenElements[j] = i;
+                    }
+                }
+
+            }
+
+            int[] output = null;
+            for (int i = 0; i < indexElementSameGivenElements.Length; i++)
+            {
+                output = DeleteElementWithIndexInArray(input, indexElementSameGivenElements[i]);
+            }
+
+            return DeleteElementInArray(element, output);
+        }
+
+        //delete element with index in given array
+        //input : given an array, given index
+        //output : return an array after delete element with index in given array
+        public static int[] DeleteElementWithIndexInArray(int[] input, int index)
+        {
+            if (index < 0 || index > input.Length)
+            {
+                return input;
+            };
+            int[] output = new int[input.Length - 1];
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (i < index)
+                {
+                    output[i] = input[i];
+                }
+                else
+                {
+                    output[i] = input[i + 1];
+                }
+
+            }
+            return output;
+        }
+
+        //edit elements in array with given value
+        //input : given an array, given value
+        //output : return an array after edit elements with given value
+        public static int[] EditGivenElementWithValueInArray(int pos, int value, int[] input)
+        {
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (i == pos)
+                {
+                    input[i] = value;
+                }
+            }
+            return input;
+        }
+
+        //delete element duplicate in given array
+        //input : given an array
+        //output : return an array after delete element duplicate in given array
+        public static int[] DeleteElementDuplicateInArray(int[] input, int index)
+        {
+            if (index < 0 || index > input.Length)
+            {
+                return input;
+            };
+            int[] output = new int[input.Length - 1];
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (i < index)
+                {
+                    output[i] = input[i];
+                }
+                else
+                {
+                    output[i] = input[i + 1];
+                }
+
+            }
+            return output;
+        }
+    }
+}
---
+++ DataStructureAlgorithmsCSharp/TestCase.cs
@@ -0,0 +1,21 @@
+﻿using DataStructure;
+using System;
+
+namespace Test
+{
+    public class TestCase
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = {1,2,7,2,5,6,3,2,9};
+            Console.WriteLine("After delete element has value 5");
+
+            for (int i = 0; i < ArrayStructure.EditGivenElementWithValueInArray(2,8,input).Length; i++)
+            {
+                Console.WriteLine(ArrayStructure.EditGivenElementWithValueInArray(2, 8, input)[i]);
+
+            }
+
+        }
+    }
+}
